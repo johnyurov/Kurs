6bcf6743d1349d062596fda35bd23198b80ecb3e
YUrov <52971293+johnyurov@users.noreply.github.com>
2019-09-23 03:10:53 +0700

    Update Source.cpp
    10 тема

--- LW/Source.cpp
+++ LW/Source.cpp
@@ -183,7 +183,20 @@ void CHOOSE_THEME()
 		}
 		case 10:
 		{
-
+			cout << "\nВы выбрали десятую тему, ниже представлени список слов.\n\nОзнакомьтесь с ним и выучите представленные слова." << endl << endl;
+			char words[256];
+			char ch = ':';
+			string path = "природа.txt";
+			ifstream theme_one;
+			theme_one.open(path);
+			if (theme_one)
+			{
+				while (!theme_one.eof())
+				{
+					theme_one.getline(words, 256, ch);
+					cout << words << endl;
+				}
+			}
 			break;
 		}
 		default:
