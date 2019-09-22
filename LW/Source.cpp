f1f878e584f3414feb4e660e831bf108dcdb7423
YUrov <52971293+johnyurov@users.noreply.github.com>
2019-09-23 02:30:23 +0700

    Update Source.cpp
    3 тема

--- LW/Source.cpp
+++ LW/Source.cpp
@@ -57,7 +57,20 @@ void CHOOSE_THEME()
 		}
 		case 3:
 		{
-
+			cout << "\nВы выбрали третью тему, ниже представлени список слов.\n\nОзнакомьтесь с ним и выучите представленные слова." << endl << endl;
+			char words[256];
+			char ch = ':';
+			string path = "прилагательные.txt";
+			ifstream theme_one;
+			theme_one.open(path);
+			if (theme_one)
+			{
+				while (!theme_one.eof())
+				{
+					theme_one.getline(words, 256, ch);
+					cout << words<<endl;
+				}
+			}
 			break;
 		}
 		case 4:
